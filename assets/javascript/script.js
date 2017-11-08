d209da68725805920c8a15c647609ca9d776bc1d
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-08 05:14:38 +0000

    init

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+console.log("Script.js connected")
+
+function updateQuestion() {
+    $("#mainArea").html();
+    $("#").text();
+    $("#").text();
+    $("#").text();
+}
+
+//I have to create an object which will store questions, and answers
+//and also I have to create a function (constructor), which will take questions, and put it in the object(As I did with The Matrix RPG game).
+
+//For displaying content - I will use mainArea div, and jquery .html method.
+//To take the answer - I will create button-listener, and get data-correct atribute - if it 1 - show the answer, and go futher.
